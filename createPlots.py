506fe9ac5e10d4045d94e5b75e06f4670ac696c1
geralttrzcianaka@gmail.com <luna12345>
2018-11-14 14:23:14 +0100

    quality extended

--- createPlots.py
+++ createPlots.py
@@ -1,14 +1,10 @@
-'''from subprocess import Popen, PIPE, STDOUT
-import io
-p = Popen(['java', '-jar', '.\\out\\artifacts\\ATSP_jar\\ATSP.jar','test1','greedy','general'], stdout=PIPE, stderr=STDOUT)
-for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):
-    print(line)'''
 import subprocess
 import os
 import json
 import math
 import statistics
 import matplotlib.pyplot as plt
+import matplotlib
 from itertools import cycle
 import re
 import numpy as np
@@ -107,18 +103,18 @@ def calculateQualityAndDeviation(algorithms=["greedy","steepest","simpeheuristic
             time=f.readlines()[1].strip()
             f.close()
             if(float(data[1][1])==math.inf):
-                writer.write(b[0]+";inf;inf;0;"+time+"\n")
+                writer.write(b[0]+";inf;inf;0;"+time+";"+data[1][4]+";"+data[1][5]+"\n")
             else:
                 bestResult=float(data[1][1])/theBest-1
                 avgResult=float(data[1][2])/theBest-1
                 deviation=statistics.stdev([float(x[1])/theBest-1 for x in data[2:]])
-                writer.write(b[0]+";"+str(bestResult)+";"+str(avgResult)+";"+str(deviation)+";"+time+"\n")
+                writer.write(b[0]+";"+str(bestResult)+";"+str(avgResult)+";"+str(deviation)+";"+time+";"+data[1][4]+";"+data[1][5]+"\n")
         writer.close()
-
 #calculateQualityAndDeviation()
 
 #2.1
 #2.2
+#2.4
 #jakość - (solution-optimum)/optimum im mniejsze tym lepsze 0==optimum
 def createQualityPlot(algorithms=["greedy","steepest","simpeheuristic","random"],type="best"):
     cycol = cycle('brgcmk')
@@ -130,6 +126,10 @@ def createQualityPlot(algorithms=["greedy","steepest","simpeheuristic","random"]
         data=4
     elif(type=="avg"):
         data=2
+    elif(type=="iterations"):
+        data=5
+    elif(type=="evaluatedSolutions"):
+        data=6
     i=0
     for algo in algorithms:
         f = open(".\\solutions\\"+algo+"\\Quality.txt", "r")
@@ -152,11 +152,16 @@ def createQualityPlot(algorithms=["greedy","steepest","simpeheuristic","random"]
     plt.show()
 
 #albo skala logarytmiczna albo podzielic wykresy
+#2.1
 #createQualityPlot(type="best")
 #createQualityPlot(type="avg")
+#2.2
 #createQualityPlot(type="time")
+#2.4
+#createQualityPlot(algorithms=["greedy","steepest"],type="iterations")
+#createQualityPlot(algorithms=["greedy","steepest"],type="evaluatedSolutions")
 
-#2.4 jakosc/czas (jakosc/sekunde)
+#2.3 jakosc/czas (jakosc/sekunde)
 def createQualityInTimePlot(algorithms=["greedy","steepest","simpeheuristic","random"],best=True,avg=True):
     marker = cycle('*s^opx')
     fig, ax = plt.subplots()
@@ -183,9 +188,9 @@ def createQualityInTimePlot(algorithms=["greedy","steepest","simpeheuristic","ra
     ax.legend()
     fig.tight_layout()
     plt.show()
-createQualityInTimePlot()
-createQualityInTimePlot(algorithms=["greedy","steepest"],best=False)
-createQualityInTimePlot(algorithms=["greedy","steepest"],avg=False)
+#createQualityInTimePlot()
+#createQualityInTimePlot(algorithms=["greedy","steepest"],best=False)
+#createQualityInTimePlot(algorithms=["greedy","steepest"],avg=False)
 
 #3
 def createQualityStartAndEndPlot():
@@ -235,32 +240,23 @@ def createQualityToRunNumberPlot():
         fig.tight_layout()
         plt.show()
 #createQualityToRunNumberPlot()
-'''
+
 #TODO podobienstwo samych permutacji
 def createPermutationSimilarityPlot():
-    cycol = cycle('brgcmk')
-    marker = "."
-    f = open(".\\solutions\\bests.txt", "r")
-    bests=[b.strip().split(";") for b in f.readlines()]
-    f.close()
     for nameOfFile in [x for x in os.listdir(".\\solutions\\greedy") if x.startswith("multi_300_")]:
-        fig, ax = plt.subplots()
-        theBest=[float(b[1]) for b in bests if b[0]==nameOfFile[10:-4]][0]
         file = open(".\\solutions\\greedy\\"+nameOfFile, "r")
         data=file.readlines()
         file.close()
         name=data[0].strip()
         solutions=[d.strip().split(";") for d in data[1:301]]
         x=range(1,301)
-        v=[float(x[2])/theBest-1 for x in solutions]
-        y=[min(v[:i]) for i in x]
-        y2=[statistics.mean(v[:i]) for i in x]
-        ax.plot(x,y, next(cycol)+marker,linestyle="--",label=name)
-        ax.plot(x,y2, next(cycol)+marker,linestyle="--",label=name)
-        ax.legend()
-        fig.tight_layout()
+        y=range(1,301)
+        x = np.random.random(20)
+        y = np.random.random(20)
+        z = np.random.randint(-2,3,20)
+        cmap=plt.cm.rainbow
+        #norm = matplotlib.colors.BoundaryNorm(np.arange(-2.5,3,1), cmap.N)
+        plt.scatter(x,y,c=z,cmap=cmap,s=100,edgecolor='none',marker='s')
+        plt.colorbar(ticks=np.linspace(-2,2,5))
         plt.show()
-'''
-
-
-#TODO liczba krokow/ocen trzeba wyciagnac i zrobic wykresy 
+createPermutationSimilarityPlot()
